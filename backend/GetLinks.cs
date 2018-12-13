d7c62746ceeec54d3086d10f37eeff93a0dee73d
Cecil Phillip <cecilphillip@APEX-MACBOOK-2018.local>
2018-12-13 12:58:16 -0500

    [backend] add GetLinks operation

---
+++ backend/GetLinks.cs
@@ -0,0 +1,35 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace LinkyLink
+{
+    public static partial class LinkOperations
+    {
+        [FunctionName("GetLinks")]
+        public static IActionResult GetLinks(
+            [HttpTrigger(AuthorizationLevel.Function, "GET", Route = "links/{vanityUrl}")] HttpRequest req,
+            [CosmosDB(
+                databaseName: "linkylinkdb",
+                collectionName: "linkbundles",
+                ConnectionStringSetting = "LinkLinkConnection",
+                SqlQuery = "SELECT * FROM linkbundles lb WHERE lb.vanityUrl = {vanityUrl}"
+            )] IEnumerable<LinkBundle> documents,
+            string vanityUrl,
+            ILogger log)
+        {
+            if (!documents.Any()) return new NotFoundResult();
+
+            LinkBundle doc = documents.Single();
+            return new OkObjectResult(doc);
+        }
+    }
+}
