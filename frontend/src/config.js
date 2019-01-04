7c07589e8dd65a3da6f2bcea615e5475a7e1f826
Burke Holland <burkeholland@Burkes-MBP.lan>
2019-01-04 09:40:30 -0600

    removing cached files

--- frontend/src/config.js
+++
@@ -1,8 +0,0 @@
-export default {
-    API_URL: `${process.env.VUE_APP_API}/api`,
-    scraper: process.env.VUE_APP_SCRAPER,
-    functionKey: process.env.VUE_APP_FUNCTION_KEY,
-    AUTH_URL: `${process.env.VUE_APP_API}/${process.env.VUE_APP_AUTH_URL}`,
-    ME_URL: `${process.env.VUE_APP_API}/${process.env.VUE_APP_ME_URL}`
-};
-//# sourceMappingURL=config.js.map
--- frontend/src/shared/Array.js
+++
@@ -1,6 +0,0 @@
-Array.prototype.get = function (id) {
-    let index = this.findIndex(x => x.id === id);
-    return { item: this[index], index: index };
-};
-export default Array;
-//# sourceMappingURL=Array.js.map
