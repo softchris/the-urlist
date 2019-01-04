f320effcfbbf76b08f72b6238414dd269b6a44f1
Burke Holland <burkeholland@Burkes-MBP.lan>
2019-01-04 09:34:34 -0600

    Remove progress bar

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,13 +2,6 @@ import Vue from "vue";
 import App from "./App.vue";
 import router from "./router";
 import { store } from "./store/store";
-const VueProgressBar = require("vue-progressbar");
-
-Vue.use(VueProgressBar, {
-  color: "rgb(143, 255, 199)",
-  failedColor: "red",
-  height: "2px"
-});
 
 Vue.config.productionTip = false;
 
