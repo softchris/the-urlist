a2d193347e807642f628038f289da46f3daa87a5
Burke Holland <burkeholland@Burkes-MBP.lan>
2019-02-20 16:34:03 -0600

    Always return user to home page on login

--- frontend/src/config.ts
+++ frontend/src/config.ts
@@ -6,7 +6,7 @@ export default {
       process.env.VUE_APP_API
     }/.auth/login/${provider}?post_login_redirect_url=${
       process.env.VUE_APP_APP_URL
-    }${returnPath}`;
+    }`;
   },
   FUNCTION_KEY: process.env.VUE_APP_FUNCTION_KEY,
   ME_URL: `${process.env.VUE_APP_API}/${process.env.VUE_APP_ME_URL}`
