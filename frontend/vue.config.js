4b20d00ca1513b19a520eb140e86fb8b2fcad307
Burke Holland <burkeholland@gmail.com>
2019-03-06 10:35:45 -0600

    Updated config for debugger

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -5,5 +5,18 @@ module.exports = {
         data: `@import "@/styles/site.scss";`
       }
     }
+  },
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === "development") {
+      config.devtool = "eval-source-map";
+      config.output.devtoolModuleFilenameTemplate = info =>
+        info.resourcePath.match(/\.vue$/) &&
+        !info.identifier.match(/type=script/) // this is change ✨
+          ? `webpack-generated:///${info.resourcePath}?${info.hash}`
+          : `webpack-yourCode:///${info.resourcePath}`;
+
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        "webpack:///[resource-path]?[hash]";
+    }
   }
 };
